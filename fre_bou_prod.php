a98279912312799eed76b34fc5323e2efeeaafef
Devadharshini <143801102+deva16081920@users.noreply.github.com>
2024-09-18 10:42:10 +0530

    Add files via upload

---
+++ fre_bou_prod.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:"Freaquently Bought Product"
+Plugin URI: https://localhost:8000
+Description: This is to add the frequently bought product suggestion from user's search
+Author: Devadharshini
+Version:1.1.2
+*/
+
+defined("ABSPATH") or die;
+defined("FBT_PATH") or define("FBT_PATH",plugin_dir_path(__FILE__));
+defined("FBT_URL") or define("FBT_PATH",plugin_dir_url(__FILE__));
